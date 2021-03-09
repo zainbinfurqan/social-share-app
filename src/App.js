4e22f481d11e3def772eb2545536ebf8dcd25765
zainbinfurqan <zain.ahmed199524@gmail.com>
2021-03-09 15:56:40 +0500

    this is first commit for social share app

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
 import logo from "./logo.svg";
 import "./App.css";
-import HelmetMetaData from "./lib-components/helmet";
-import { LinkedinShareButton ,LinkedinIcon} from "react-share";
+// import HelmetMetaData from "./lib-components/helmet";
+import { LinkedinShareButton, LinkedinIcon } from "react-share";
+// import SEO from './components/seo'
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <>
-    <HelmetMetaData
+      {/* <HelmetMetaData
         title={undefined}
         description={undefined}
         image={undefined}
-      ></HelmetMetaData>
-    <div className="App" style={{
-      backgroundColor:'black',
-      height:'100vh'
-    }}>
-      
-      <LinkedinShareButton
-        url={"https://social-share-app.herokuapp.com/"}
-        title={"CampersTribe - World is yours to explore"}
-        hashtag="#camperstribe"
-        //  className={classes.socialMediaButton}
+      ></HelmetMetaData> */}
+      <Helmet>
+        <meta name="description" content="Tutorial for React Helmet" />
+        <meta name="theme-color" content="#0000" />
+        <meta property="og:site_name" content="CampersTribe" />
+        <meta
+          property="url"
+          content="https://social-share-app.herokuapp.com/"
+        />
+        <meta name="description" content="Nested component"></meta>
+        <title>React Helmet Tutorial</title>
+        <link rel="canonical" href="https://social-share-app.herokuapp.com/" />
+      </Helmet>
+      <div
+        className="App"
+        style={{
+          backgroundColor: "black",
+          height: "100vh",
+        }}
       >
-        <LinkedinIcon size={32}/>
-      </LinkedinShareButton>
-    </div>
+        <LinkedinShareButton
+          url={"https://social-share-app.herokuapp.com/"}
+          title={"CampersTribe - World is yours to explore"}
+          hashtag="#camperstribe"
+          //  className={classes.socialMediaButton}
+        >
+          <LinkedinIcon size={32} />
+        </LinkedinShareButton>
+      </div>
     </>
   );
 }
